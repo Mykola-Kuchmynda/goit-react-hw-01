5618ee491fd37a9190d9fc9eee60a064d90c0f4f
Mykola Kuchmynda <mykola.kuchmynda@gmail.com>
2025-04-18 14:06:29 +0300

    task-1

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+
+import userData from "../../userData.json";
+import Profile from "../Profile/Profile"
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
